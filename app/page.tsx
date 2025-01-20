0c58b0f1f0b12c92481b1f4f454f7c5e80a43f3d
jose0229 <sanghoonim@hanyang.ac.kr>
2025-01-20 18:19:34 +0900

    [fix] 다 삭제 후 update pw만 남김

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,3 @@
-import AccordionFeatures from "./components/accordion-features";
-import BeforeAfter from "./components/before-after";
-import { Container } from "./components/container";
-import CTA from "./components/cta";
-import FAQ from "./components/faq";
-import Footer from "./components/footer";
-import { Header } from "./components/header";
-import Hero from "./components/hero";
-import LogoClouds from "./components/logo-clouds";
-import Pricing from "./components/pricing";
-import { renderSchemaTags } from "./lib/seo";
-
 export default function Home() {
-  return (
-    <Container>
-      <Header />
-      <Hero />
-      <LogoClouds />
-      <BeforeAfter />
-      <AccordionFeatures />
-      <Pricing />
-      <FAQ />
-      <CTA />
-      <Footer />
-      {renderSchemaTags()}
-    </Container>
-  );
+  return <h1> Welcome to yom. </h1>;
 }
