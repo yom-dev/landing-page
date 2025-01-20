fd237db73a664cb9d895f9e831b0f2d48a9a4139
jose0229 <sanghoonim@hanyang.ac.kr>
2025-01-20 18:26:49 +0900

    컴포넌트 모두 삭제

--- app/components/container.tsx
+++
@@ -1,8 +0,0 @@
-import clsx from "clsx";
-
-export function Container({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<"div">) {
-  return <div className={clsx("mx-auto", className)} {...props} />;
-}
